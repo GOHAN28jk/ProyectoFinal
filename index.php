6abd2839675810fa1211f16e0fcf3f4759906a5a
GOHAN28jk <84548918+GOHAN28jk@users.noreply.github.com>
2022-05-30 16:10:17 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,482 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+  <meta charset="utf-8">
+  <title>Servi Automotriz </title>
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <meta content="" name="keywords">
+  <meta content="" name="description">
+
+  <!-- Facebook Opengraph integration: https://developers.facebook.com/docs/sharing/opengraph -->
+  <meta property="og:title" content="">
+  <meta property="og:image" content="">
+  <meta property="og:url" content="">
+  <meta property="og:site_name" content="">
+  <meta property="og:description" content="">
+
+  <!-- Twitter Cards integration: https://dev.twitter.com/cards/  -->
+  <meta name="twitter:card" content="summary">
+  <meta name="twitter:site" content="">
+  <meta name="twitter:title" content="">
+  <meta name="twitter:description" content="">
+  <meta name="twitter:image" content="">
+
+  <!-- Place your favicon.ico and apple-touch-icon.png in the template root directory -->
+  <link href="favicon.ico" rel="shortcut icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,700,700i|Raleway:300,400,500,700,800" rel="stylesheet">
+
+  <!-- Bootstrap CSS File -->
+  <link href="lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Libraries CSS Files -->
+  <link href="lib/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+  <link href="lib/animate-css/animate.min.css" rel="stylesheet">
+
+  <!-- css -->
+  <link href="css/style.css" rel="stylesheet">
+  <!-- iconos-->
+  <script src="https://kit.fontawesome.com/88b773483f.js" crossorigin="anonymous"></script>
+
+</head>
+
+<body>
+  <div id="preloader"></div>
+
+  <!--==========================
+  Hero Section
+  ============================-->
+  <section id="hero">
+    <div class="hero-container">
+      <div class="wow fadeIn">
+
+
+        <h1>SERVI AUTOMOTRIZ MANUEL GUAÑUNA</h1>
+        <h2>Servicios :<span class="rotating">Correctivo y Preventivo</span></h2>
+        <div class="actions">
+          <a href="vista/login.php" class="btn-get-started">Ingresar</a>
+          
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!--==========================
+  Sección de encabezado
+  ============================-->
+  <header id="header">
+    <div class="container">
+      <div id="logo" class="pull-left">
+       <!-- <a href="#hero"><img src="img/logo.png" alt="" title="" /></img></a>-->
+        <!-- Descomenta abajo si prefieres usar una imagen de texto -->
+        <!--<h1><a href="#hero">Encabezado 1</a></h1>-->
+      </div>
+
+      <nav id="nav-menu-container">
+        <ul class="nav-menu">
+          <li class="menu-active"><a href="#hero">Inicio</a></li>
+          <li><a href="#about">Quienes somos como empresa</a></li>
+          <li><a href="#services">Servicios</a></li>
+          <li><a href="#portfolio">Portfolio</a></li>
+          <li><a href="#testimonials">Nuestros Mecáninos</a></li>
+          <li><a href="#team">Otros Servicios</a></li>
+          <li><a href="#contact">Contactos</a></li>
+          <li class="menu-has-children"><a href="vista/login.php">Login</a>  </li>
+        </ul>
+      </nav>
+      <!-- #nav-menu-container -->
+    </div>
+  </header>
+  <!-- #header -->
+
+  <!--==========================
+  About Section
+  ============================-->
+  <section id="about">
+    <div class="container wow fadeInUp">
+      <div class="row">
+        <div class="col-md-12">
+          <h3 class="section-title">Quienes somos como empresa</h3>
+          <div class="section-title-divider"></div>
+          <p class="section-description"></p>
+        </div>
+      </div>
+    </div>
+    <div class="container about-container wow fadeInUp">
+      <div class="row">
+        <div class="col-md-6 col-md-push-6 about-content">
+          <h2 class="about-title">MISIÓN </h2>
+          <p class="about-text">
+          Nuestra misión como empresa es brindar un servicio de calidad en el mantenimiento correctivo 
+            y preventivo, en vehículos a Diesel y Gasolina con profesionalismo y eficiencia que nos caracteriza
+            ,teniendo como principal objetivo lograr el reconocimiento y satisfacción de nuestros clientes.
+          </p>
+          <h2 class="about-title">VISIÓN </h2>
+          
+          <p class="about-text">
+          Ser una empresa reconocida en el mantenimiento preventivo y correctivo de vehículos livianos y pesados.
+            mejorar la infraestructura para brindar un mejor servicio a nuestros clientes a nivel nacional.
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!--==========================
+  Services Section
+  ============================-->
+  <section id="services">
+    <div class="container wow fadeInUp">
+      <div class="row">
+        <div class="col-md-12">
+          <h3 class="section-title">Servicios</h3>
+          <div class="section-title-divider"></div>
+          <p class="section-description"></p>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-md-4 service-item">
+          <div class="service-icon"><i class="fas fa-oil-can"></i></div>
+          <h4 class="service-title"><a href="">Cambio de aceite</a></h4>
+          <p class="service-description">El cambio de aceite se considera  un proceso esencial para que el vehículo funcione bien y prolongue la vida útil del vehículo.</p>
+        </div>
+        <div class="col-md-4 service-item">
+          <div class="service-icon"><i class="fa-regular fa-circle-xmark"></i></div>
+          <h4 class="service-title"><a href="">Chequeo de frenos</a></h4>
+          <p class="service-description">La seguridad del vehículo y de sus ocupantes depende en gran medida del buen funcionamiento del sistema de  frenos. Se hará el trabajo de pastilla, disco y purga necesaria del sistema hidráulico.</p>
+        </div>
+        <div class="col-md-4 service-item">
+          <div class="service-icon"><i class="fas fa-car"></i></div>
+          <h4 class="service-title"><a href="">Reparación de motor</a></h4>
+          <p class="service-description">Este es el componente que impulsa toda la operación.La reparación del motor generalmente se realiza porque el uso, los daños, la suciedad, la fricción y el desgaste hacen que las piezas pierdan la calibración, aunque esto depende de las condiciones de uso y cuidado.</p>
+        </div>
+        <div class="col-md-4 service-item">
+          <div class="service-icon"><i class="fa-solid fa-screwdriver-wrench"></i></div>
+          <h4 class="service-title"><a href="">Reparación de transmisiones</a></h4>
+          <p class="service-description">El paso del tiempo y de los kilómetros conlleva siempre un desgaste especialmente notorio de sus partes móviles, principalmente de dos tipos: transmisión automática o manual.</p>
+        </div>
+        <div class="col-md-4 service-item">
+          <div class="service-icon"><i class="fas fa-truck"></i></div>
+          <h4 class="service-title"><a href="">Reparación de diferenciales</a></h4>
+          <p class="service-description">El diferencial es una parte mecánica del automóvil que transmite la rotación del motor a las ruedas encargadas de la tracción. De esta manera, permite que los neumáticos giren a diferentes velocidades, lo cual es fundamental para un movimiento preciso en las curvas.</p>
+        </div>
+        <div class="col-md-4 service-item">
+          <div class="service-icon"><i class="fa fa-check-square-o"></i></div>
+          <h4 class="service-title"><a href="">Chequeo electrónico(ESCÁNER)</a></h4>
+          <p class="service-description">Es una herramienta que se utiliza para diagnosticar las fallas electrónicas de un auto, específicamente las almacenadas en la computadora del mismo.</p>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!--==========================
+  Subscrbe Section
+  ============================-->
+  <section id="subscribe">
+    <div class="container wow fadeInUp">
+      <div class="row">
+        <div class="col-md-8">
+          <h3 class="subscribe-title">Revisa tu vehículo en línea</h3>
+          <p class="subscribe-text"></p>¡Puedes revisar el estado de tu vehículo desde cualquier parte del mundo!
+        </div>
+        <div class="col-md-4 subscribe-btn-container">
+          <a class="subscribe-btn" href="vista/login.php">Ingresar</a>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!--==========================
+  Porfolio Section
+  ============================-->
+  <section id="portfolio">
+    <div class="container wow fadeInUp">
+      <div class="row">
+        <div class="col-md-12">
+          <h3 class="section-title">Portafolio </h3>
+          <div class="section-title-divider"></div>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-md-3">
+          <a class="portfolio-item" style="background-image: url(img/portafolio1.jpg);" >
+            <div class="details">
+              <h4>Portafolio  1</h4>
+            
+            </div>
+          </a>
+        </div>
+
+        <div class="col-md-3">
+          <a class="portfolio-item" style="background-image: url(img/portafolio2.jpg);">
+            <div class="details">
+              <h4>Portafolio  2</h4>
+             
+            </div>
+          </a>
+        </div>
+
+        <div class="col-md-3">
+          <a class="portfolio-item" style="background-image: url(img/portafolio3.jpg);">
+            <div class="details">
+              <h4>Portafolio  3</h4>
+             
+            </div>
+          </a>
+        </div>
+
+        <div class="col-md-3">
+          <a class="portfolio-item" style="background-image: url(img/portafolio4.jpg);">
+            <div class="details">
+              <h4>Portafolio  4</h4>
+            
+            </div>
+          </a>
+        </div>
+
+        <div class="col-md-3">
+          <a class="portfolio-item" style="background-image: url(img/portafolio5.jpg);" >
+            <div class="details">
+              <h4>Portafolio  5</h4>
+              
+            </div>
+          </a>
+        </div>
+
+        <div class="col-md-3">
+          <a class="portfolio-item" style="background-image: url(img/portafolio6.jpg);">
+            <div class="details">
+              <h4>Portafolio  6</h4>
+              <span>Alored dono par</span>
+            </div>
+          </a>
+        </div>
+
+        <div class="col-md-3">
+          <a class="portfolio-item" style="background-image: url(img/portafolio7.jpg);">
+            <div class="details">
+              <h4>Portafolio  7</h4>
+            
+            </div>
+          </a>
+        </div>
+
+        <div class="col-md-3">
+          <a class="portfolio-item" style="background-image: url(img/portafolio8.jpg);">
+            <div class="details">
+              <h4>Portafolio 8</h4>
+              
+            </div>
+          </a>
+        </div>
+
+      </div>
+    </div>
+  </section>
+
+  <!--==========================
+  Nuestros Mecanicos
+  ============================-->
+  <section id="testimonials">
+    <div class="container wow fadeInUp">
+      <div class="row">
+        <div class="col-md-12">
+          <h3 class="section-title">Nuestros Mecánicos</h3>
+          <div class="section-title-divider"></div>
+          <p class="section-description"></p>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-md-3">
+          <div class="profile">
+            <div class="pic"><img src="img/mecanicoManuel.jpg" alt=""></div>
+            <h4>Manuel Guañuna</h4>
+            <span>Mecánico Automotriz</span>
+          </div>
+        </div>
+        <div class="col-md-9">
+          <div class="quote">
+            <b><img src="img/quote_sign_left.png" alt=""></b> El gerente propietario Manuel Guañuna tiene
+             gran trayectoria en referencia en su trabajo ya que ha laborado en diferentes empresas 
+             como:MOTRANSA,MITSUBISHI,SICO-MERCEDES BENZ,TURBAUTO,AUTEC,NEO-TOMAS,TROLE BUS,SECOHI
+             demostrando profesionalismo y dedicación en su trabajo.Ha realizado diferentes cursos
+             de capacitación tanto nacionales como internacionales que le han brindado el conocimiento
+             y la experiencia para el desempeño de su profesión.<small><img src="img/quote_sign_right.png" alt=""></small>
+          </div>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-md-9">
+          <div class="quote">
+            <b><img src="img/quote_sign_left.png" alt=""></b> Labora en la empresa, demostrando capacidad y 
+           responsabilidad en cada uno de los trabajos encomendados gracias a sus conocimientos en el aria 
+           de mecánica automotriz <small><img src="img/quote_sign_right.png" alt=""></small>
+          </div>
+        </div>
+        <div class="col-md-3">
+          <div class="profile">
+            <div class="pic"><img src="img/mecanicoMauricio.jpg" alt=""></div>
+            <h4>Mauro Condor</h4>
+            <span>Bachiller Automotriz</span>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </section>
+
+  <!--==========================
+  Team Section
+  ============================-->
+  <section id="team">
+    <div class="container wow fadeInUp">
+      <div class="row">
+        <div class="col-md-12">
+          <h3 class="section-title">Otros Servicios</h3>
+          <div class="section-title-divider"></div>
+          <p class="section-description"></p>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-md-3">
+          <div class="member">
+            <div class="pic"><img src="img/portafolio8.jpg" alt=""></div>
+            <h4>Servicio de escáner</h4>
+            <span></span>
+
+          </div>
+        </div>
+
+        <div class="col-md-3">
+          <div class="member">
+            <div class="pic"><img src="img/asesoriamiento.jpg" alt=""></div>
+            <h4>Asesoramiento técnico</h4>
+            <span></span>
+          
+          </div>
+        </div>
+
+        <div class="col-md-3">
+          <div class="member">
+            <div class="pic"><img src="img/imagendemotor.jpg" alt=""></div>
+            <h4>Repuestos</h4>
+            <span></span>
+         
+          </div>
+        </div>
+
+        <div class="col-md-3">
+          <div class="member">
+            <div class="pic"><img src="img/seccion_de_servicio.jpg" alt=""></div>
+            <h4>Auxilio mecánico</h4>
+            <span></span>
+           
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </section>
+
+  <!--==========================
+  Contact Section
+  ============================-->
+  <section id="contact">
+    <div class="container wow fadeInUp">
+      <div class="row">
+        <div class="col-md-12">
+          <h3 class="section-title">Contactos</h3>
+          <div class="section-title-divider"></div>
+          <p class="section-description"></p>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-md-3 col-md-push-2">
+          <div class="info">
+            <div>
+              <i class="fa fa-map-marker"></i>
+              <p>Carlos Mantilla<br>Feliciano Checa</p>
+            </div>
+
+            <div>
+              <i class="fa fa-envelope"></i>
+              <p>m.isaias-1970@hotmail.com</p>
+            </div>
+
+            <div>
+              <i class="fa fa-phone"></i>
+              <p>+593 0998785044 </p>
+            </div>
+            <div>
+              <i class="fa fa-clock-o"></i>
+              <p> Horario de atención 8 a.m - 18 p.m  </p>
+            </div>
+            <div class="social">
+               <a href="https://www.facebook.com/Servicio-Automotriz-Manuel-Gua%C3%B1una-Paredes-2239930709377242/"><i class="fa fa-facebook"></i></a>
+               <p>Facebook</p>
+            </div>
+         
+            
+
+          </div>
+        </div>
+
+    
+        <div class="col-md-5 col-md-push-2">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3989.8155582566915!2d-78.43643538533051!3d-0.06741859995080704!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x91d589adf5df8489%3A0x71df6fa6154a3fa0!2sMec%C3%A1nica%20Manuel%20Gua%C3%B1una!5e0!3m2!1ses!2sec!4v1651697215959!5m2!1ses!2sec" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+
+        </div>
+
+      </div>
+    </div>
+  </section>
+
+  <!--==========================
+  Footer
+============================-->
+  <footer id="footer">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="copyright">
+            &copy; <strong>johan_Guañuna</strong>
+          </div>
+          <div class="credits">
+        
+            Facebook <a href="https://www.facebook.com/Servicio-Automotriz-Manuel-Gua%C3%B1una-Paredes-2239930709377242/">Facebook</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+  <!-- #footer -->
+
+  <a href="#" class="back-to-top"><i class="fa fa-chevron-up"></i></a>
+
+  <!-- Required JavaScript Libraries -->
+  <script src="lib/jquery/jquery.min.js"></script>
+  <script src="lib/bootstrap/js/bootstrap.min.js"></script>
+  <script src="lib/superfish/hoverIntent.js"></script>
+  <script src="lib/superfish/superfish.min.js"></script>
+  <script src="lib/morphext/morphext.min.js"></script>
+  <script src="lib/wow/wow.min.js"></script>
+  <script src="lib/stickyjs/sticky.js"></script>
+  <script src="lib/easing/easing.js"></script>
+
+  <!-- Template Specisifc Custom Javascript File -->
+  <script src="js/custom.js"></script>
+
+  <script src="contactform/contactform.js"></script>
+
+
+</body>
+
+</html>
